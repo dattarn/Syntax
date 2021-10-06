a8935d2e2fbbd8cfb5d4acc6e515988123c49a4e
dattarn <86503920+dattarn@users.noreply.github.com>
2021-10-06 16:38:32 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,38 @@
+package basic.dev;
+/** 
+*	Ghi chuì (comment nhiêÌu doÌng) 
+*	@author TranThanhBinh 
+*	1. Tên lõìp truÌng tên file 
+*	2. Tên lõìp chıŞ caìi ğâÌu viêìt hoa. Nêìu tıÌ gheìp thiÌ nhıŞng chıŞ caìi dâÌu viêìt hoa 
+*	4. Tên lõìp ğãòt phaÒi coì yì nghiŞa. Viì duò: class Peson : lõìp mô taÒ con ngıõÌi 
+*	- Interface: PhaÒi coì tiêÌn tôì I. Viì duò: IListener  * - Abstract: PhaÒi coì tiêÌn tôì Abstract. Viì duò: 
+AbstractShape 
+ */ 
+
+public class MainApp {
+	//Comment 1 doÌng caìch 1. Phiìm noìng Ctr + / 
+	 	/*Comment 1 doÌng caìch 2*/  	public 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 		System.out.println("Hello World"); 
+	 	 	/** 
+*	Caìch ğãòt tên biêìn: 
+*	1. Không chıìa kyì tıò ğãòc biêòt !@#$%^&*()-+= 
+*	2. Không chıìa khoaÒng trãìng 
+*	3. Viêìt thıõÌng 
+*	4. Không ğıõòc bãìt ğâÌu bãÌng sôì 
+	 	 	 */ 
+	 	 	int number; 
+	 	 	String nameSchool; 
+	 	} 
+ 	 
+	 	/* 
+*	tên phıõng thıìc tuân theo quy tãìc camelCase 
+	 	 */ 
+	 	public void show() { 
+ 	 	 
+	 	} 
+} 
+}
+
+
